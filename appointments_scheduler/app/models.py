ff44ba3ccad27820b687ecb66bcbda9e98a8c687
Vinícius Chan <viniciuschan@hotmail.com>
2017-12-09 20:15:08 -0200

    Criação da model e serializer

--- appointments_scheduler/app/models.py
+++ appointments_scheduler/app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    """ Representative class for the patient table in database. """
+    
+    SEX_CHOICES = (
+        ('F', 'Female'),
+        ('M', 'Male')
+    )
+
+    name = models.CharField(max_length=250, verbose_name='Nome')
+    birthdate = models.DateField(max_length=3, verbose_name='Data de nascimento')
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, verbose_name='Sexo')
+    phone = models.CharField(max_length=20, verbose_name='Telefone')
+    email = models.EmailField(verbose_name='E-mail")
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Paciente'
+
+class Procedure(models.Model):
+    """ Representative class for the procedure table in database """
+
+    description = models.CharField(max_length=100, verbose_name='Procedimento')
+    cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Valor')
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        verbose_name = 'Procedimento'
+
+class Appointment(models.Model):
+    """ Representative class for the appointment table in database """
+
+    patient = models.ForeignKey("Patient", related_name="patient")
+    procedure = models.ForeignKey("Procedure", related_name="procedure")
+    date = models.DateField()
+    start_at = models.DateTimeField(null=False)
+    end_at = models.DateTimeField()
+
+    def __str__(self):
+        return "{} - {}, às {}".format(self.patient.name, self.procedure.description, self.start_at)
+    
+    class Meta:
+        verbose_name = 'Agendamento'
---
+++ appointments_scheduler/app/serializers.py
@@ -0,0 +1,33 @@
+from .models import Appointment, Patient, Procedure
+
+from rest_framework import serializers
+
+
+class PatientSerializer(serializers.HyperlinkedModelSerializer):
+    """ Patient model serializer """
+    
+    class Meta:
+        model = Patient
+        fields = ("id", "name", "birthdate", "sex", "phone", "email")
+        read_only_fields = ("id",)
+
+
+class ProcedureSerializer(serializers.HyperlinkedModelSerializer):
+    """ Procedure model serializer """
+
+    class Meta:
+        model = Procedure
+        fields = ("id", "description", "cost")
+        read_only_fields = ("id")
+
+
+class AppointmentSerializer(serializers.HyperlinkedModelSerializer):
+    """ Appointment model serializer """
+
+    patient = PatientSerializer
+    procedure = ProcedureSerializer
+    read_only_fields = ("id")
+
+    class Meta:
+        model = Appointment
+        fields = ("id", "patient", "procedure", "date", "start_at", "end_at")
