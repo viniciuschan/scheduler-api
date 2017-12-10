6c5e3d084512d9a2360f4c89c4b6a2d24b9578e0
Vinícius Chan <viniciuschan@hotmail.com>
2017-12-10 00:29:17 -0200

    Editado arquivo admin.py

--- appointments_scheduler/app/admin.py
+++ appointments_scheduler/app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Appointment, Patient, Procedure
 
-# Register your models here.
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['patient', 'procedure', 'date', 'start_at', 'end_at']
+
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'sex', 'birthdate', 'phone', 'email']
+
+
+class ProcedureAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'cost']
+
+
+admin.site.register(Appointment, AppointmentAdmin)
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Procedure, ProcedureAdmin)
