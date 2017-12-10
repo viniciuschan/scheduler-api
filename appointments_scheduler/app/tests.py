5eb0eb761fb4c113727b1a4f47aaaef0155399f7
Vinícius Chan <viniciuschan@hotmail.com>
2017-12-10 04:30:07 -0200

    Adicionado alguns testes

--- appointments_scheduler/app/tests.py
+++ appointments_scheduler/app/tests.py
@@ -9,44 +9,67 @@ class PatientTestCase(APITransactionTestCase):
 
         Patient.objects.create(
             id=1,
-            name="Vinicius Chan",
-            birthdate="1990-09-07",
-            sex="M",
-            phone="16981211201",
-            email="viniciuschan@hotmail.com"
+            name="Test User",
+            birthdate="1995-12-09",
+            sex="F",
+            phone="16998882809",
+            email="test2@email.com"
         )
 
         Patient.objects.create(
             id=2,
-            name="Marcela Correa",
-            birthdate="1996-11-05",
+            name="Michael William",
+            birthdate="2017-12-10",
             sex="F",
-            phone="16982827373",
-            email="marcela@hotmail.com"
+            phone="16923232323",
+            email="michael@hotmail.com"
         )
 
 
     def test_patient_list(self):
         
         response = self.client.get("/patients/")
-
+        
         self.assertEqual(response.json(),
             [
                 {
-                    'id'=1,
-                    'name'='Vinicius Chan',
-                    'birthdate'='1990-09-07',
-                    'sex'='M',
-                    'phone'='16981211201',
-                    'email'='viniciuschan@hotmail.com'
+                    "id": 1,
+                    "name": "Test User",
+                    "birthdate": "1995-12-09",
+                    "sex": "F",
+                    "phone": "16998882809",
+                    "email": "test2@email.com"
                 },
                 {
-                    'id'=2,
-                    'name'='Marcela Correa',
-                    'birthdate'='1996-11-05',
-                    'sex'='F',
-                    'phone'='16982827373',
-                    'email'='marcela@hotmail.com'
+                    "id": 2,
+                    "name": "Michael William",
+                    "birthdate": "2017-12-10",
+                    "sex": "F",
+                    "phone": "16923232323",
+                    "email": "michael@hotmail.com"
                 }
             ]
         )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_patient_create(self):
+        
+        payload = {
+            "id": 1,
+            "name": "Test User",
+            "birthdate": "1995-12-09",
+            "sex": "F",
+            "phone": "16998882809",
+            "email": "test2@email.com"
+        }
+
+        response = self.client.post("/patients/", payload)
+        self.assertEqual(response.status_code, 201)
+
+
+    def test_patient_delete(self):
+        response = self.client.delete("/patients/1/")
+        self.assertEqual(response.status_code, 204)
+
+
