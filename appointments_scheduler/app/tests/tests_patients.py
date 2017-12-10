f7a8fe2fea4b96a7a3487b0559f59c8e4a5b5423
Vinícius Chan <viniciuschan@hotmail.com>
2017-12-10 02:56:51 -0200

    fix

--- appointments_scheduler/app/tests/tests_patients.py
+++ appointments_scheduler/app/tests/tests_patients.py
@@ -16,7 +16,7 @@ class PatientTestCase(APITransactionTestCase):
             email="viniciuschan@hotmail.com"
         )
 
-        Patient.objects.create(
+            Patient.objects.create(
             id=2,
             name="Marcela Correa",
             birthdate="1996-11-05",
@@ -24,3 +24,22 @@ class PatientTestCase(APITransactionTestCase):
             phone="16982827373",
             email="marcela@hotmail.com"
         )
+
+
+    def test_patient_list(self):
+        
+        response = self.client.get("/patients/")
+        
+            [
+                {
+                    'id'='1',
+                    'name'='Vinicius Chan',
+                    'birthdate'='1990-09-07',
+                    'sex'='M',
+                    'phone'='16981211201',
+                    'email'='viniciuschan@hotmail.com'
+                }
+            ]
+        )
+
+        print(response)
