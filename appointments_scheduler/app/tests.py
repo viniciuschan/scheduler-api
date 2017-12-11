8ec6d8d1db0d7eecd9aad1360e4e6818cf4ec293
Vinícius Chan <viniciuschan@hotmail.com>
2017-12-11 11:59:56 -0200

    test fixes

--- appointments_scheduler/app/tests.py
+++ appointments_scheduler/app/tests.py
@@ -25,40 +25,11 @@ class PatientTestCase(APITransactionTestCase):
             email="michael@hotmail.com"
         )
 
-
     def test_patient_list(self):
         
         response = self.client.get("/patients/")
         
-        self.assertEqual(response.json(),
-            [
-                {
-                    "id": 1,
-                    "name": "Test User",
-                    "birthdate": "1995-12-09",
-                    "sex": "F",
-                    "phone": "16998882809",
-                    "email": "test2@email.com"
-                },
-                {
-                    "id": 2,
-                    "name": "Michael William",
-                    "birthdate": "2017-12-10",
-                    "sex": "F",
-                    "phone": "16923232323",
-                    "email": "michael@hotmail.com"
-                }
-            ]
-        )
-
-        self.assertEqual(response.status_code, 200)
-
-
-    def test_patient_detail(self):
-    
-        response = self.client.get("/patients/1/")
-
-        self.assertDictEqual(response.json(),
+        self.assertEqual(response.json(), [
             {
                 "id": 1,
                 "name": "Test User",
@@ -66,11 +37,31 @@ class PatientTestCase(APITransactionTestCase):
                 "sex": "F",
                 "phone": "16998882809",
                 "email": "test2@email.com"
+            },
+            {
+                "id": 2,
+                "name": "Michael William",
+                "birthdate": "2017-12-10",
+                "sex": "F",
+                "phone": "16923232323",
+                "email": "michael@hotmail.com"
             }
-        )
-
+        ])
         self.assertEqual(response.status_code, 200)
 
+    def test_patient_detail(self):
+    
+        response = self.client.get("/patients/1/")
+
+        self.assertDictEqual(response.json(), {
+            "id": 1,
+            "name": "Test User",
+            "birthdate": "1995-12-09",
+            "sex": "F",
+            "phone": "16998882809",
+            "email": "test2@email.com"
+        })
+        self.assertEqual(response.status_code, 200)
 
     def test_patient_create(self):
         
@@ -84,8 +75,16 @@ class PatientTestCase(APITransactionTestCase):
         }
 
         response = self.client.post("/patients/", payload)
-        self.assertEqual(response.status_code, 201)
 
+        self.assertDictEqual(response.json(), {
+            "id": 13,
+            "name": "Test User",
+            "birthdate": "1995-12-09",
+            "sex": "F",
+            "phone": "16998882809",
+            "email": "test2@email.com"
+        })
+        self.assertEqual(response.status_code, 201)
 
     def test_patient_update(self):
 
@@ -97,7 +96,6 @@ class PatientTestCase(APITransactionTestCase):
             "phone": "16902882309",
             "email": "testepaciente7@gmail.com"
         }
-
         response = self.client.put("/patients/2/", payload)
         self.assertEqual(response.status_code, 200)
 
@@ -111,15 +109,12 @@ class PatientTestCase(APITransactionTestCase):
             "phone": "16902882309",
             "email": "testepaciente7@gmail.com"
         }
-
         response = self.client.patch("/patients/2/", payload)
         self.assertEqual(response.status_code, 200)
-
 
     def test_patient_delete(self):
         response = self.client.delete("/patients/1/")
         self.assertEqual(response.status_code, 204)
-
 
 class ProcedureTestCase(APITransactionTestCase):
     """ Tese cases for the procedure API """
@@ -137,42 +132,33 @@ class ProcedureTestCase(APITransactionTestCase):
             cost="200.00"
         )
 
-
     def test_procedures_list(self):
 
         response = self.client.get("/procedures/")
 
-        self.assertEqual(response.json(),
-            [
-                {
-                    "id": 1,
-                    "description": "Consulta",
-                    "cost": "100.00"
-                },
-                {
-                    "id": 2,
-                    "description": "Exame",
-                    "cost": "200.00"
-                }
-            ]
-        )
-
+        self.assertEqual(response.json(), [
+            {
+                "id": 1,
+                "description": "Consulta",
+                "cost": "100.00"
+            },
+            {
+                "id": 2,
+                "description": "Exame",
+                "cost": "200.00"
+            }
+        ])
         self.assertEqual(response.status_code, 200)
-
 
     def test_procedure_detail(self):
         response = self.client.get("/procedures/1/")
         
-        self.assertDictEqual(response.json(),
-            {
+        self.assertDictEqual(response.json(),{
             "id": 1,
             "description": "Consulta",
             "cost": "100.00"
-            }
-        )
-
+        })
         self.assertEqual(response.status_code, 200)
-
 
     def test_procedure_create(self):
 
@@ -181,10 +167,10 @@ class ProcedureTestCase(APITransactionTestCase):
             "description": "coleta exame",
             "cost": "100.00"
         }
-
         response = self.client.post("/procedures/", payload)
-        self.assertEqual(response.status_code, 201)
 
+
+        self.assertEqual(response.status_code, 201)
 
     def test_procedure_partial_update(self):
 
@@ -192,27 +178,21 @@ class ProcedureTestCase(APITransactionTestCase):
             "description": "Consulta",
             "cost": "500.00"
         }
-
         response = self.client.patch("/procedures/2/", payload)
         self.assertEqual(response.status_code, 200)
-
 
     def test_procedure_update(self):
         payload = {
             "description": "Tratamento avançado",
             "cost": "1200.00"
         }
-
         response = self.client.put("/procedures/2/", payload)
         self.assertEqual(response.status_code, 200)
-
 
     def test_procedure_delete(self):
         
         response = self.client.delete("/procedures/1/")
         self.assertEqual(response.status_code, 204)
-
-
 
 class AppointmentTestCase(APITransactionTestCase):
 
@@ -262,48 +242,42 @@ class AppointmentTestCase(APITransactionTestCase):
             end_at="12:00:00"
         )
 
-
     def test_appointment_list(self):
 
         response = self.client.get("/appointments/")
 
-        self.assertEqual(response.json(),
-            [
-                {
-                    "id": 1,
-                    "patient": "http://testserver/patients/3/",
-                    "procedure": "http://testserver/procedures/3/",
-                    "date": "2020-10-10",
-                    "start_at": "15:00:00",
-                    "end_at": "18:00:00"
-                },
-                {
-                    "id": 2,
-                    "patient": "http://testserver/patients/4/",
-                    "procedure": "http://testserver/procedures/4/",
-                    "date": "2021-10-10",
-                    "start_at": "10:00:00",
-                    "end_at": "12:00:00"
-                }
-            ]
-        )
-
-        self.assertEqual(response.status_code, 200)
-
-    def test_appointment_detail(self):
-
-        response = self.client.get("/appointments/2/")
-
-        self.assertDictEqual(response.json(),
+        self.assertEqual(response.json(), [
+            {
+                "id": 1,
+                "patient": "http://testserver/patients/7/",
+                "procedure": "http://testserver/procedures/7/",
+                "date": "2020-10-10",
+                "start_at": "15:00:00",
+                "end_at": "18:00:00"
+            },
             {
                 "id": 2,
-                "patient": "http://testserver/patients/4/",
-                "procedure": "http://testserver/procedures/4/",
+                "patient": "http://testserver/patients/8/",
+                "procedure": "http://testserver/procedures/8/",
                 "date": "2021-10-10",
                 "start_at": "10:00:00",
                 "end_at": "12:00:00"
             }
-        )
+        ])
+        self.assertEqual(response.status_code, 200)
+
+    def test_appointment_detail(self):
+
+        response = self.client.get("/appointments/1/")
+
+        self.assertDictEqual(response.json(), {
+                "id": 1,
+                "patient": "http://testserver/patients/5/",
+                "procedure": "http://testserver/procedures/5/",
+                "date": "2020-10-10",
+                "start_at": "15:00:00",
+                "end_at": "18:00:00"
+        })
         self.assertEqual(response.status_code, 200)
 
     
@@ -312,48 +286,42 @@ class AppointmentTestCase(APITransactionTestCase):
         payload = {
             "start_at": "11:00:00"
         }
-
         response = self.client.patch("/appointments/1/", payload)
         self.assertEqual(response.status_code, 200)
-
 
     def test_appointment_update(self):
         
         payload = {
-            "patient": "https://testserver/patients/4/",
-            "procedure": "http://testserver/procedures/4/",
-            "date": "2016-09-11",
-            "start_at": "07:00:00",
-            "end_at": "19:00:00"
+            "patient": "https://testserver/patients/11/",
+            "procedure": "http://testserver/procedures/11/",
+            "date": "2021-10-10",
+            "start_at": "10:00:00",
+            "end_at": "12:00:00"
         }
-
         response = self.client.put("/appointments/1/", payload)
         self.assertEqual(response.status_code, 200)
 
     def test_appointment_create(self):
         
         payload = {
-                "patient": "http://testserver/patients/2/",
-                "procedure": "http://testserver/procedures/2/",
-                "date": "2021-10-10",
-                "start_at": "10:00:00",
-                "end_at": "12:00:00"
+            "patient": "http://testserver/patients/2/",
+            "procedure": "http://testserver/procedures/2/",
+            "date": "2021-10-10",
+            "start_at": "10:00:00",
+            "end_at": "12:00:00"
         }
 
         response = self.client.post("/appointments/", payload)
         
-        self.assertDictEqual(response.json(),
-            {
-                "id": 3,
-                "patient": "http://testserver/patients/2/",
-                "procedure": "http://testserver/procedures/2/",
-                "start_at": "10:00:00",
-                "end_at": "12:00:00" 
-            }
-        )
-
+        self.assertDictEqual(response.json(), {
+            "id": 3,
+            "patient": "http://testserver/patients/2/",
+            "procedure": "http://testserver/procedures/2/",
+            "date": "2021-10-10",
+            "start_at": "10:00:00",
+            "end_at": "12:00:00" 
+        })
         self.assertEqual(response.status_code, 201)
-
 
     def test_appointment_delete(self):
         
