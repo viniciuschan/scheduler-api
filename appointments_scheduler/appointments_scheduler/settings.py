b57ec02827041b504ca2eb38680f953f5350297d
Vinicius Chan <viniciuschan@hotmail.com>
2018-03-05 17:52:32 -0300

    Fixed unicode issue

--- appointments_scheduler/appointments_scheduler/settings.py
+++ appointments_scheduler/appointments_scheduler/settings.py
@@ -45,7 +45,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
     #    'rest_framework.permissions.IsAdminUser',
     ],
-    'PAGE_SIZE': 10
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
 }
 
 MIDDLEWARE = [
