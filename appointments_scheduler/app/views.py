496aa4b275dac52b21c5de71f0d17a3e8a9669b1
Vinicius Chan <viniciuschan@hotmail.com>
2019-04-15 21:06:56 -0300

    fix lines

--- appointments_scheduler/app/views.py
+++ appointments_scheduler/app/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
 from .models import Appointment, Patient, Procedure
 from .serializers import AppointmentSerializer, PatientSerializer, ProcedureSerializer
 
-from rest_framework import viewsets
-from rest_framework.response import Response
 
 class AppointmentViewSet(viewsets.ModelViewSet):
     """ API endpoint that allows appointments to be viewed or edited. """
+
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
 
